ab8e4b3d98f6a18ccb26ac2fba0341951f912226
lsotofdev <luis.soto@pucp.pe>
2021-06-29 01:40:50 -0500

    ADD: Discount Grpc

---
+++ src/Services/Discount/Discount.Grpc/Mapper/DiscountProfile.cs
@@ -0,0 +1,16 @@
+// DiscountProfile.cs01:0601:06
+
+using AutoMapper;
+using Discount.Grpc.Entities;
+using Discount.Grpc.Protos;
+
+namespace Discount.Grpc.Mapper
+{
+    public class DiscountProfile : Profile
+    {
+        public DiscountProfile()
+        {
+            CreateMap<Coupon, CouponModel>().ReverseMap();
+        }
+    }
+}
